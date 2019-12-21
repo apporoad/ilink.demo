51a9c5fd003842ca81a30d0401e6ce59be07ae35
apporoad <apporoadina@163.com>
2019-12-21 12:29:16 +0800

    all

---
+++ index.js
@@ -0,0 +1,6 @@
+var saySome = require('./saySome')
+
+saySome.hello()
+console.log('name:' + saySome.name)
+saySome.version()
+console.log('==========================')
---
+++ saySome.js
@@ -0,0 +1,7 @@
+require('ilink.js').reg(module,'ilink.demo')
+
+exports.hello= ()=>{}
+
+exports.name = "origin"
+
+exports.version= ()=>{}
